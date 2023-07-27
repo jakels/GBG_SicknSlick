51fe5545158525ef04b1de9cc9467cf65ef0b7bb
ValleyHound <ll22jls@leeds.ac.uk>
2023-07-27 16:49:48 +0100

    Filled more media

--- unique_assets/js/navbar.js
+++ unique_assets/js/navbar.js
@@ -30,9 +30,9 @@ var writeDOM = `
                     <li class="nav-item">
                     <a class="nav-link [active page insert contactus.html]" aria-current="page" href="contactus.html">Contact Us</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link [active page insert links.html]" aria-current="page" href="links.html">Links</a>
-                    </li>
+                    <!--<li class="nav-item">-->
+                    <!--<a class="nav-link [active page insert links.html]" aria-current="page" href="links.html">Links</a>-->
+                    <!--</li>-->
                 </ul>
             </div>
         </div>
