e61cded73901c3f12cc285da9ccd82ec87f105ad
ValleyHound <ll22jls@leeds.ac.uk>
2023-07-28 14:13:38 +0100

    Finished filling in media, added "videos" & "links" pages

--- unique_assets/js/footer_wizard.js
+++ unique_assets/js/footer_wizard.js
@@ -4,7 +4,7 @@ var writeDOM_footerWizard = `
 
 <footer class="container">
     <p class="float-end"><a href="#">Back to top</a></p>
-    <p>&copy; 1998 - <span id="fillYear"></span> SicknSlick LTD &middot; Website Developed & Designed by GreyBird Group &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <p>&copy; 1998 - <span id="fillYear"></span> SicknSlick LTD &middot; Website Developed & Designed by GreyBird Group</p>
     <script>document.getElementById("fillYear").innerHTML = new Date().getFullYear();</script>
 </footer>
 
--- unique_assets/js/navbar.js
+++ unique_assets/js/navbar.js
@@ -9,7 +9,7 @@ var writeDOM = `
 <header data-bs-theme="dark">
     <nav class="navbar navbar-expand-md fixed-top" style="background-color: rgb(20, 18, 18);">
         <div class="container-fluid">
-            <img src="/unique_assets/images/site_media/logo_sicknslick_text_truthful.png" style="height: 40px; margin-right: 25px;">
+            <a href="index.html"><img src="/unique_assets/images/site_media/logo_sicknslick_text_truthful.png" style="height: 40px; margin-right: 25px;"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -19,7 +19,10 @@ var writeDOM = `
                     <a class="nav-link [active page insert index.html]" aria-current="page" href="index.html">Home</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link [active page insert gallery.html]" aria-current="page" href="gallery.html">Gallery</a>
+                    <a class="nav-link [active page insert gallery.html]" aria-current="page" href="gallery.html">Pictures</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link [active page insert videos.html]" aria-current="page" href="videos.html">Videos</a>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link [active page insert reviews.html]" aria-current="page" href="reviews.html">Reviews</a>
@@ -30,9 +33,9 @@ var writeDOM = `
                     <li class="nav-item">
                     <a class="nav-link [active page insert contactus.html]" aria-current="page" href="contactus.html">Contact Us</a>
                     </li>
-                    <!--<li class="nav-item">-->
-                    <!--<a class="nav-link [active page insert links.html]" aria-current="page" href="links.html">Links</a>-->
-                    <!--</li>-->
+                    <li class="nav-item">
+                    <a class="nav-link [active page insert links.html]" aria-current="page" href="links.html">Links</a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -49,6 +52,7 @@ writeDOM = writeDOM.replace("[active page insert pressreleases.html]", "");
 writeDOM = writeDOM.replace("[active page insert contactus.html]", "");
 writeDOM = writeDOM.replace("[active page insert links.html]", "");
 writeDOM = writeDOM.replace("[active page insert reviews.html]", "");
+writeDOM = writeDOM.replace("[active page insert videos.html]", "");
 
 console.log(writeDOM);
 
